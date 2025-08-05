856aa9d63810edb5f2c7e469c96fd123639e487d
Matheus Bernardino <mmtheus69@gmail.com>
2025-08-05 17:46:04 -0300

    Menu do dashboard mobile finalizado

--- src/app/(panel)/dashboard/_components/sidebar.tsx
+++ src/app/(panel)/dashboard/_components/sidebar.tsx
@@ -1,26 +1,174 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { usePathname } from "next/navigation"
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 import { clsx } from "clsx";
-
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import {
+  Banknote,
+  CalendarCheck,
+  ChevronLeft,
+  ChevronRight,
+  Folder,
+  List,
+  Settings,
+} from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import LogoImg from "../../../../../public/logo-odonto.png";
 
 export function SidebarDashboard({ children }: { children: React.ReactNode }) {
-
   const pathname = usePathname();
   const [isCollapsed, setIsCollapsed] = useState(false);
 
-
-
-
-
   return (
     <div className="flex min-h-screen w-full">
-      
-      <div className={}>
+      <aside
+        className={clsx(
+          "flex flex-col border-r bg-background transition-all duration-300 p-4 h-full",
+          {
+            "w-20": isCollapsed,
+            "w-64": !isCollapsed,
+            "hidden md:flex md:fixed": true,
+          }
+        )}
+      >
+        <div className="mb-6 mt-4">
+          {!isCollapsed && (
+            <Image
+              src={LogoImg}
+              alt="Logo do OdontoPRO"
+              priority
+              quality={100}
+              style={{
+                width: "auto",
+                height: "auto",
+              }}
+            />
+          )}
+        </div>
 
+        <Button
+          className="bg-gray-100 hover:bg-gray-50 text-zinc-900 self-end mb-2"
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          {!isCollapsed ? (
+            <ChevronLeft className="w-12 h-12" />
+          ) : (
+            <ChevronRight className="w-12 h-12" />
+          )}
+        </Button>
+      </aside>
+
+      <div
+        className={clsx("flex flex-1 flex-col transition-all duration-300", {
+          "md:ml-20": isCollapsed,
+          "md:ml-64": !isCollapsed,
+        })}
+      >
+        <header className="md:hidden flex items-center justify-between border-b px-2 md:px-6 h-14 z-10 sticky top-0 bg-white">
+          <Sheet>
+            <div className="flex items-center gap-4">
+              <SheetTrigger>
+                <Button
+                  variant="outline"
+                  asChild
+                  size="icon"
+                  className="md:hidden"
+                >
+                  <List className="w-5 h-5" />
+                </Button>
+              </SheetTrigger>
+
+              <h1 className="text-base md:text-lg font-semibold">
+                Menu OdontoPRO
+              </h1>
+            </div>
+
+            <SheetContent
+              side="right"
+              className="sm:max-w-xs text-black px-4 pt-5"
+            >
+              <SheetTitle>OdontoPRO</SheetTitle>
+              <SheetDescription>Menu Administrativo</SheetDescription>
+
+              <nav className="grid gap-2 text-base pt-5">
+                <SidebarLink
+                  href="/dashboard"
+                  label="Agendamentos"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<CalendarCheck className="w-6 h-6" />}
+                />
+                <SidebarLink
+                  href="/dashboard/services"
+                  label="Serviços"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<Folder className="w-6 h-6" />}
+                />
+
+                <SidebarLink
+                  href="/dashboard/profile"
+                  label="Configurações"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<Settings className="w-6 h-6" />}
+                />
+                <SidebarLink
+                  href="/dashboard/plans"
+                  label="Planos"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<Banknote className="w-6 h-6" />}
+                />
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </header>
+
+        <main className="flex-1 py-4 px-2 md:p-6">{children}</main>
       </div>
-
     </div>
-  )
+  );
+}
+
+interface SidebarLinkProps {
+  href: string;
+  icon: React.ReactNode;
+  label: string;
+  pathname: string;
+  isCollapsed: boolean;
+}
+
+function SidebarLink({
+  href,
+  icon,
+  label,
+  pathname,
+  isCollapsed,
+}: SidebarLinkProps) {
+  return (
+    <Link href={href}>
+      <div
+        className={clsx(
+          "flex items-center gap-2 px-3 py-2 rounded-md transition-colors",
+          {
+            "bg-blue-500 text-white": pathname === href,
+            "hover:bg-gray-100 text-gray-700": pathname !== href,
+          }
+        )}
+      >
+        <span className="w-6 h-6">{icon}</span>
+        {!isCollapsed && <span>{label}</span>}
+      </div>
+    </Link>
+  );
 }
--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -2,7 +2,6 @@ export default function Dashboard() {
   return (
     <div>
       <h1>Welcome to the Page Dashboard</h1>
-      <p>This is a simple page component.</p>
     </div>
   );
 }
