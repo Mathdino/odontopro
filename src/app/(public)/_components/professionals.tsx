568155084961b0c23c655168d25335a99bc7d7e0
Matheus Bernardino <mmtheus69@gmail.com>
2025-09-09 11:22:55 -0300

    ajuste no link da page inicial

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -1,27 +1,27 @@
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
-import fotoImg from "../../../../public/foto1.png"
+import fotoImg from "../../../../public/foto1.png";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 
-export function Professionals (){
+export function Professionals() {
   return (
-   <section className="bg-gray-50 py-16">
+    <section className="bg-gray-50 py-16">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className="text-3xl text-center mb-12 font-bold">
+          Clinicas Disponíveis
+        </h2>
 
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 className="text-3xl text-center mb-12 font-bold">Clinicas Disponíveis</h2>
-
-      <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        
-        <Card className="overflow-hidden py-0">
+        <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          <Card className="overflow-hidden py-0">
             <CardContent className="p-0">
               <div>
                 <div className="relative h-48">
-                  <Image 
-                  src={fotoImg}
-                  alt="Foto da logo da clinica"
-                  fill
-                  className="object-cover"
+                  <Image
+                    src={fotoImg}
+                    alt="Foto da logo da clinica"
+                    fill
+                    className="object-cover"
                   />
                 </div>
               </div>
@@ -30,21 +30,25 @@ export function Professionals (){
                 <div className="flex items-center justify-between">
                   <div>
                     <h3 className="font-semibold">Clinica Centro</h3>
-                    <p className="text-sm text-gray-500">Rua x, centro, São Paulo - SP</p>
+                    <p className="text-sm text-gray-500">
+                      Rua x, centro, São Paulo - SP
+                    </p>
                   </div>
                   <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
                 </div>
 
-                <Link href="/clinica/123" className="w-full bg-emerald-500 hover:bg-emerald-600 text-white flex items-center justify-center py-2 rounded-md text-sm font-medium md:text-base">
+                <Link
+                  href="/clinica/cmf1h9z080000orv8bfkdix0u"
+                  className="w-full bg-emerald-500 hover:bg-emerald-600 text-white flex items-center justify-center py-2 rounded-md text-sm font-medium md:text-base"
+                >
                   Agendar Horário
-                  <ArrowRight className="ml-2"/>
+                  <ArrowRight className="ml-2" />
                 </Link>
               </div>
             </CardContent>
-        </Card>
-        
-      </section>
-    </div>
-   </section>
-  )
+          </Card>
+        </section>
+      </div>
+    </section>
+  );
 }
