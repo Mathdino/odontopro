e8656df3f2d1928bf416466da3f4c2df0c4581d4
Matheus Bernardino <mmtheus69@gmail.com>
2025-09-10 15:41:44 -0300

    Exclusão de horários agendados

--- src/app/(panel)/dashboard/_components/appointents/appointents-list.tsx
+++ src/app/(panel)/dashboard/_components/appointents/appointents-list.tsx
@@ -9,10 +9,13 @@ import {
   CardDescription,
 } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { format } from "date-fns";
 import { Prisma } from "@prisma/client";
-import { Phone } from "lucide-react";
+import { Check, Eye, Phone, X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cancelAppointment } from "../../_actions/cancel-appointment";
+import { toast } from "sonner";
 
 type AppointmentWithService = Prisma.AppointmentGetPayload<{
   include: {
@@ -28,8 +31,9 @@ export function AppointentsList({ times }: AppointentsListProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const date = searchParams.get("date");
+  const queryClient = useQueryClient();
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, refetch } = useQuery({
     queryKey: ["get-appointments", date],
     queryFn: async () => {
       let activeDate = date;
@@ -50,6 +54,9 @@ export function AppointentsList({ times }: AppointentsListProps) {
       }
       return json;
     },
+
+    staleTime: 20000,
+    refetchInterval: 25000,
   });
 
   const occupantMap: Record<string, AppointmentWithService[]> = {};
@@ -72,6 +79,21 @@ export function AppointentsList({ times }: AppointentsListProps) {
     }
   }
 
+  async function handleCancelAppointement(appointmentId: string) {
+    const response = await cancelAppointment({
+      appointmentId,
+    });
+
+    if (response.error) {
+      toast.error(response.error);
+      return;
+    }
+
+    queryClient.invalidateQueries({ queryKey: ["get-appointments"] });
+    await refetch();
+    toast.success(response.data);
+  }
+
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -83,42 +105,66 @@ export function AppointentsList({ times }: AppointentsListProps) {
       </CardHeader>
 
       <CardContent>
-        <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4"></ScrollArea>
-        {isLoading ? (
-          <p>Carregando Agenda...</p>
-        ) : (
-          times.map((slot) => {
-            const occupant = occupantMap[slot];
+        <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
+          {isLoading ? (
+            <p>Carregando Agenda...</p>
+          ) : (
+            times.map((slot) => {
+              const occupant = occupantMap[slot];
 
-            if (occupant) {
+              if (occupant) {
+                return (
+                  <div
+                    key={slot}
+                    className="flex items-center py-2 border-t last:border-b"
+                  >
+                    <div className="w-16 text-sm font-semibold">{slot}</div>
+                    <div className="flex-1 text-sm">
+                      <div className="font-semibold mb-1">{occupant.name}</div>
+                      <div className="text-sm flex items-center text-gray-500">
+                        <Phone className="w-4 h-4 mr-2" />
+                        {occupant.phone}
+                      </div>
+                    </div>
+
+                    <div className="ml-auto">
+                      <div className="flex gap-1">
+                        <Button variant="ghost" size="icon">
+                          <Eye className="w-4 h-4" />
+                        </Button>
+
+                        <Button
+                          className="bg-emerald-500 text-white hover:bg-emerald-400"
+                          size="icon"
+                        >
+                          <Check className="w-4 h-4" />
+                        </Button>
+
+                        <Button
+                          className="bg-red-500 text-white hover:bg-red-400"
+                          onClick={() => handleCancelAppointement(occupant.id)}
+                          size="icon"
+                        >
+                          <X className="w-4 h-4" />
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+
               return (
                 <div
                   key={slot}
                   className="flex items-center py-2 border-t last:border-b"
                 >
                   <div className="w-16 text-sm font-semibold">{slot}</div>
-                  <div className="flex-1 text-sm">
-                    <div className="font-semibold mb-1">{occupant.name}</div>
-                    <div className="text-sm flex items-center text-gray-500">
-                      <Phone className="w-4 h-4 mr-2" />
-                      {occupant.phone}
-                    </div>
-                  </div>
+                  <div className="flex-1 text-sm text-gray-500">Disponível</div>
                 </div>
               );
-            }
-
-            return (
-              <div
-                key={slot}
-                className="flex items-center py-2 border-t last:border-b"
-              >
-                <div className="w-16 text-sm font-semibold">{slot}</div>
-                <div className="flex-1 text-sm text-gray-500">Disponível</div>
-              </div>
-            );
-          })
-        )}
+            })
+          )}
+        </ScrollArea>
       </CardContent>
     </Card>
   );
