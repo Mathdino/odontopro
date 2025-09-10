ebaf9f5c8a06a7af7e8529ace2c5c7a8bc80e7c7
Matheus Bernardino <mmtheus69@gmail.com>
2025-09-10 12:28:54 -0300

    Agendamentos com informações

--- src/app/(panel)/dashboard/_components/appointents/appointents-list.tsx
+++ src/app/(panel)/dashboard/_components/appointents/appointents-list.tsx
@@ -11,6 +11,14 @@ import {
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
+import { Prisma } from "@prisma/client";
+import { Phone } from "lucide-react";
+
+type AppointmentWithService = Prisma.AppointmentGetPayload<{
+  include: {
+    service: true;
+  };
+}>;
 
 interface AppointentsListProps {
   times: string[];
@@ -35,7 +43,7 @@ export function AppointentsList({ times }: AppointentsListProps) {
 
       const response = await fetch(url);
 
-      const json = await response.json();
+      const json = (await response.json()) as AppointmentWithService[];
 
       if (!response.ok) {
         return [];
@@ -43,6 +51,26 @@ export function AppointentsList({ times }: AppointentsListProps) {
       return json;
     },
   });
+
+  const occupantMap: Record<string, AppointmentWithService[]> = {};
+
+  if (data && data.length > 0) {
+    for (const appointment of data) {
+      const requiredSlots = Math.ceil(appointment.service.duration / 30);
+
+      const startIndex = times.indexOf(appointment.time);
+
+      if (startIndex !== -1) {
+        for (let i = 0; i < requiredSlots; i++) {
+          const slotIndex = startIndex + i;
+
+          if (slotIndex < times.length) {
+            occupantMap[times[slotIndex]] = appointment;
+          }
+        }
+      }
+    }
+  }
 
   return (
     <Card>
@@ -55,8 +83,31 @@ export function AppointentsList({ times }: AppointentsListProps) {
       </CardHeader>
 
       <CardContent>
-        <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
-          {times.map((slot) => {
+        <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4"></ScrollArea>
+        {isLoading ? (
+          <p>Carregando Agenda...</p>
+        ) : (
+          times.map((slot) => {
+            const occupant = occupantMap[slot];
+
+            if (occupant) {
+              return (
+                <div
+                  key={slot}
+                  className="flex items-center py-2 border-t last:border-b"
+                >
+                  <div className="w-16 text-sm font-semibold">{slot}</div>
+                  <div className="flex-1 text-sm">
+                    <div className="font-semibold mb-1">{occupant.name}</div>
+                    <div className="text-sm flex items-center text-gray-500">
+                      <Phone className="w-4 h-4 mr-2" />
+                      {occupant.phone}
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+
             return (
               <div
                 key={slot}
@@ -66,8 +117,8 @@ export function AppointentsList({ times }: AppointentsListProps) {
                 <div className="flex-1 text-sm text-gray-500">Disponível</div>
               </div>
             );
-          })}
-        </ScrollArea>
+          })
+        )}
       </CardContent>
     </Card>
   );
